8e7137b6dea46055575541ac5bb9ae8a47fb22da
Alelozanodesouza <alelozanodm@gmail.com>
2026-05-20 22:02:12 -0300

    link atualizado

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ DATABASE_URL = "sqlite:///./jogos.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
 
-origins = "web-production-f551c.up.railway.app"
+origins = "https://web-production-f551c.up.railway.app"
 
 app.add_middleware(
     CORSMiddleware,
